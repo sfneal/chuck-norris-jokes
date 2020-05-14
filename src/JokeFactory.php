e2079ab66c7c495284eff45873abee1ed72354c4
Stephen Neal <stephen.neal14@gmail.com>
2020-05-14 15:28:49 -0400

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -11,7 +11,7 @@ class JokeFactory
         'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
         'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
         "Chuck Norris' tears cure cancer. Too bad he has never cried.",
-        'Chuck Norris counted to infinity... Twice.'
+        'Chuck Norris counted to infinity... Twice.',
     ];
 
     public function __construct(array $jokes = null)
@@ -19,7 +19,8 @@ class JokeFactory
         $this->jokes = $jokes ?? $this->jokes;
     }
 
-    public function getRandomJoke() {
+    public function getRandomJoke()
+    {
         return $this->jokes[array_rand($this->jokes)];
     }
 }
