a5509b3e315841d34dba4b18c032e6e263bef878
mrstephenneal <stephen.neal14@gmail.com>
2020-05-15 12:27:11 -0400

    MAKE ChuckNorris Facade & add "extra" to composer.json

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -8,5 +8,15 @@ use Illuminate\Support\ServiceProvider;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
 
+    }
+
+    public function register()
+    {
+        $this->app->bind('chuck-norris', function () {
+            return new JokeFactory();
+        });
+    }
 }
