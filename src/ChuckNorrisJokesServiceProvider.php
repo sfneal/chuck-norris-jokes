8f6eac812abbc941a345b00a466cb33a67fea2f2
mrstephenneal <stephen.neal14@gmail.com>
2020-05-15 14:36:13 -0400

    ADD console command

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,11 +3,17 @@
 namespace Sfneal\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Sfneal\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ChuckNorrisJoke::class
+            ]);
+        }
     }
 
     public function register()
---
+++ src/Console/ChuckNorrisJoke.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sfneal\ChuckNorrisJokes\Console;
+
+
+use Illuminate\Console\Command;
+use Sfneal\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class ChuckNorrisJoke extends Command
+{
+    protected $signature = 'chuck-norris';
+
+    protected $description = 'Output a funny Chuck Norris Joke';
+
+    public function handle()
+    {
+        $this->info(ChuckNorris::getRandomJoke());
+    }
+}
